e160fe47575db52233cee6fa2290e0b195533bff
Curtis Wolterding <curtiswolterding@gmail.com>
2013-08-23 19:36:05 -0400

    Added a handler for batch processing of addons and writing/reading to/from a database. Added functionality for checking and comparing version numbers of installed and available addons.

--- addon.py
+++ addon.py
@@ -1,76 +1,142 @@
 import requests
 import re
+import zipfile
+
+def find_on_page(url, ms):
+    """
+    Return of list of FIRST matches of match_string found
+    in the next of a webpage.
+
+    If none are found, return False.
+    
+    Arguments:
+    - `url`:
+    """
+    
+    # ms = "http://addons\.curse\.cursecdn\.com/files/.*\.zip"
+    page = requests.get(url)
+
+    fa = re.findall(ms, page.text)
+    if fa:
+        return fa[0]
+    else:
+        return False
+
+def newer_version(available_version, current_version):
+    """
+    Compare two version, keeping in mind the current version might be None.
+    Arguments:
+    - `available_version`:
+    - `current_version`:
+    """
+
+    # if we have a valid available_version, but no current_version
+    # return True
+    if available_version and not current_version:
+        return True
+    elif available_version > current_version:
+        return True
+    else:
+        return False
+    
 
 
 class Addon(object):
     """
     """
     
-    def __init__(self, name):
+    def __init__(self, name, newest_file=None, installed=False):
         """
         
         Arguments:
         - `name`:
         """
         self.name = name
+        self.newest_file = newest_file
+        self.installed = installed
 
         # the addon has a 'homepage' as well as a download page
         addon_base_url = "http://www.curse.com/addons/wow/"
         self.url = addon_base_url + self.name + "/"
         self.download_url = self.url + "download/"
 
-        # where to save the file
-        # TODO: versioning? tmpfile?
-
-    def matchVersion(self):
+        self.zipfile = None
+        
+        
+    def updateAvailable(self):
         """
-        Figure out current version of an addon.
+        Check for an update. Return the version number of
+        the latest file if it is greater than our current number.
+
+        Return false if there is no newer version available, or we
+        fail to get a version for this addon.
 
         Arguments:
         - `self`:
         """
-        True
+        ms = "Newest File: ([0-9\.]+)"
+        available_version = find_on_page(self.url, ms)
 
-    def matchZipFile(self):
-        """
-        Parse a webpage looking for a zipfile for an addon.
-        
-        Return the URL if a file is found, or False if not.
-
-        Arguments:
-        - `self`: 
-        """
-
-        ms = "http://addons\.curse\.cursecdn\.com/files/.*\.zip"
-        page = requests.get(self.download_url)
-
-        fa = re.findall(ms, page.text)
-        if fa:
-            return fa[0]
+        if available_version:
+            if newer_version(available_version, self.newest_file):
+                print(available_version)
+                return available_version
+            else:
+                return False
+                                
         else:
+            print("Unable to find newest file listed for %s" % self.name)
             return False
+
 
     def getFile(self, target_file):
         """
-        Download the file to a target dir
+        Parse a webpage looking for a zipfile for an addon.
+        If a file is found, download it. Else Fail.
         
         Arguments:
         - `self`:
         """
-        zipfile = self.matchZipFile()
-        if zipfile:
+        ms = "http://addons\.curse\.cursecdn\.com/files/.*\.zip"
+        z = find_on_page(self.download_url, ms)
+        if z:
             
-            file_stream = requests.get(zipfile, stream = True)
+            file_stream = requests.get(z, stream = True)
 
             with open(target_file, 'wb') as f:
                 for chunk in file_stream.iter_content(chunk_size = 1024):
                     if chunk:
                         f.write(chunk)
                         f.flush()
+
+            # objectify our file.
+            self.zipfile = zipfile.ZipFile(target_file)
+
+            return True
+            
         else:
             # error
             print("Unable to find file for %s" % self.name)
-
-    
+            return False
             
 
+    def unzipFile(self, target_dir):
+        """
+        Extract the contents of our zipfile into the target directory.
+        
+        Arguments:
+        - `self`:
+        """
+
+        if not self.zipfile:
+            print("No zipfile!!!!")
+            return False
+
+        # TODO: try/except here, validation of file contents
+        #  to prevent traversal exploits
+        self.zipfile.extractall(target_dir)
+        return True
+
+    
+
+quartz = Addon("quartz")    
---
+++ manager.py
@@ -0,0 +1,147 @@
+## addonsync ##
+## syncronize wow addons from curse.com/wow based on URL ##
+
+import os
+import sqlite3 as db
+from addon import Addon
+
+## TODO:
+## - Wrapper for ensuring directory creation if necessary ??
+## - Time-delay to prevent curse.com from detecting automation and blocking
+## - cache for downloads (prevent too much downloading).
+## - threading (help with net and file io bottlenecks).
+## - Logging/Printing output for debugging.
+## - VERSION CHECKING
+
+## TESTING:
+## - version / updating
+## - failure on invalid item name, bad url, no file, etc..
+## - nested files (addon packs).
+
+## Target directory
+# addons_dir = "/home/curtis/.local/share/wineprefixes/wowtrial/drive_c/Program Files (x86)/World of Warcraft/Interface/AddOns"
+
+
+## Temporary hard-coded list of addons
+## this will be later read from a config-file/DB
+addon_links = [
+    "http://www.curse.com/addons/wow/omni-cc/download"
+    "http://www.curse.com/addons/wow/tidy-plates/download"
+    "http://www.curse.com/addons/wow/battlegroundtargets/download"
+    "http://www.curse.com/addons/wow/quartz/download"
+    "http://www.curse.com/addons/wow/reforgelite/download"
+    "http://www.curse.com/addons/wow/gladius/download"
+    "http://www.curse.com/addons/wow/tip-tac/download"
+    "http://www.wowinterface.com/downloads/download21377-PlateBuff-Mop"]
+
+class Manager(object):
+    """
+    Handles the updating and installation of Addons.
+    """
+    
+    def __init__(self, ):
+        """
+        """
+
+        ## file locations
+        ## TODO: argument pass this?
+        self.addons_dir = "/home/curtis/Desktop/Addons/"
+        self.temp_dir = "/tmp/addonsync/"
+
+        for i in [self.addons_dir, self.temp_dir]:
+            if not os.path.isdir(i):
+                os.mkdir(i)
+                
+
+        ## connect to DB
+        self.db = "/home/curtis/Code/Python/addonsync/addon.db"
+        self.conn = db.connect(self.db)
+
+    def installAddon(self, addon):
+        """
+        Download, and extract an addon into the addons folder.
+        
+        Arguments:
+        - `self`:
+        - `addon`:
+        """
+
+        if addon.installed:
+            #update... assume new version number only
+            with self.conn:
+                self.conn.execute("UPDATE addons SET version=? WHERE name=?;",\
+                                  (addon.newest_file, addon.name, ))
+            
+        else:
+            # add new
+            # TODO: better naming of tmp/zipfile (should end with .zip)
+            tmpfile = self.temp_dir + addon.name + ".zip"
+            if addon.getFile(tmpfile) and addon.unzipFile(self.addons_dir):
+                addon.newest_file = addon.updateAvailable
+                with self.conn:
+                    self.conn.execute("INSERT INTO addons VALUES (?, ?, ?)",\
+                                      (addon.name, addon.newest_file, True, ))
+
+
+    def updateAddons(self):
+        """
+        For all the addons in the addon db, check their version number
+        and download status, and install any addons that have updates available
+        or have never been installed.
+        
+        Arguments:
+        - `self`:
+        """
+         
+        with self.conn:
+            cur = self.conn.execute("SELECT * FROM addons;")
+            for addon in cur:
+                addon = Addon(*addon)
+
+                # check to see if an update is available
+                # if the addon isn't installed at all, we will
+                # still use the version fetched by this function
+                # to record.
+                print("Checking for updates to %s" % addon.name)
+                available_version = addon.updateAvailable()
+                if available_version or not addon.installed:
+                    addon.newest_file = available_version
+                    print("Upate available, installing %s." % addon.name)
+                    self.installAddon(addon)
+                    
+                else:
+                    print("%s is up to date." % addon.name)
+
+    def listInstalledAddons(self):
+        """
+        List all currently installed addons.
+        
+        Arguments:
+        - `self`:
+        """
+        with self.conn:
+            installed_addons = self.conn.execute("SELECT * FROM addons;")
+            for addon in installed_addons:
+                addon = Addon(*addon)
+                print("%s - version: %s" % (addon.name, addon.newest_file))
+
+    def uninstallAddon(self, addon):
+        """
+        Uninstall an Addon.
+        
+        Arguments:
+        - `self`:
+        - `addon`:
+        """
+        print("Manually do this for now :)")
+
+        
+
+        
+
+
+    
+
+        
+
+        
